9fc157fb5e1a4c7ca84c594f52cc2843fc368f00
Kram-Serg <bild003@yandex.ru>
2024-09-23 12:45:53 +0700

    Update Module_9_1.py

--- Module_9_1.py
+++ Module_9_1.py
@@ -1,8 +1,12 @@
 def apply_all_func(int_list, *functions):
     results = {}
     for function in functions:
-        results[function.__name__] = function(int_list)
+        try:
+            results[function.__name__] = function(int_list)
+        except:
+            print(f'Для функции {function.__name__} переданы не верные данные')
     return results
 
 print(apply_all_func([6, 20, 15, 9], max, min))
 print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
+print(apply_all_func([6, 20, 15, 'a'], len, sum, sorted))
